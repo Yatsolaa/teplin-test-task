566743988ecb4a506538f300e7281da79edda171
Andrii Yatsola <andrii@refactorplus.com>
2025-03-20 15:17:08 +0200

    added mega menu section content, functionality and styles

--- assets/component-custom-mega-menu.js
+++ assets/component-custom-mega-menu.js
@@ -7,10 +7,44 @@ if (!customElements.get('c-mega-menu')) {
       }
 
       connectedCallback() {
-        console.log("init");
+        this.modal = this.querySelector('.js-mega-menu__modal');
+        this.megaMenuTriggers = this.querySelectorAll('.js-mega-menu__trigger');
+        this.megaMenuTabs = this.querySelectorAll('.js-mega-menu__tab');
+
+        this.megaMenuTriggers.forEach(link => {
+          link.addEventListener('mouseover', this.modalHandler)
+        })
       }
 
       disconnectedCallback() {
+        this.megaMenuTriggers.forEach(link => {
+          link.addEventListener('mouseover', this.modalHandler)
+        })
+      }
+
+      modalHandler = (event) => {
+        this.modal.classList.add('is-active');
+
+        const currentLink = event.target.closest('.js-mega-menu__trigger');
+        const currentLinkHandle = currentLink.dataset.linkHandle;
+
+        this.megaMenuTabs.forEach(tab => {
+          tab.classList.remove('is-active');
+          if(tab.dataset.linkHandle === currentLinkHandle){
+            this.megaMenuTriggers.forEach(link => link.classList.remove('is-active'));
+            currentLink.classList.add('is-active');
+            
+            tab.classList.add('is-active');
+            tab.addEventListener('mouseleave', this.closeTabHandler);
+          }
+        })
+      }
+
+      closeTabHandler = (event) => {
+        this.megaMenuTriggers.forEach(link => link.classList.remove('is-active'));
+        this.modal.classList.remove('is-active');
+        const currentTab = event.target.closest('.js-mega-menu__tab');
+        currentTab.classList.remove('is-active');
       }
     }
   );
